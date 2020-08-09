57fbc2ef90739a583ee2e35ac808b70508f4ee35
chuzhong <352995096@qq.com>
2020-08-09 15:16:13 +0800

    switch 组件，hover 之后向右滚动

--- src/Doc/SwitchDemo.vue
+++ src/Doc/SwitchDemo.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    Switch Demo
+    <Switch/>
   </div>
 </template>
+
+<script lang="ts">
+  import Switch from '../lib/Switch.vue';
+
+  export default {
+    components: {Switch}
+  };
+</script>
