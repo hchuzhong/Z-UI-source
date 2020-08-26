01e6fa23bcc83f4e88def1b09c115469f0c3a8a2
chuzhong <352995096@qq.com>
2020-08-26 23:45:32 +0800

    完成 Dialog 页面和样式以及点击关闭功能

--- src/Doc/DialogDemo.vue
+++ src/Doc/DialogDemo.vue
@@ -1,5 +1,28 @@
 <template>
   <div>
-    Dialog Demo
+    <div>Dialog 示例</div>
+    <h1>示例1</h1>
+    <Button @click="toggle">toggle</Button>
+    <Dialog v-model:visible="x" :ok="f1" :cancel="f2" />
   </div>
 </template>
+
+<script lang="ts">
+import Vue, { ref } from "vue";
+import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
+export default {
+  components: { Dialog, Button },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const f1 = () => {
+      return false;
+    };
+    const f2 = () => {};
+    return { x, toggle, f1, f2 };
+  },
+};
+</script>
