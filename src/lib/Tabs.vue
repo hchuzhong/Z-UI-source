928292b69d47a19767d5d7c4b50079cb6e6a0348
chuzhong <352995096@qq.com>
2020-09-11 08:39:02 +0800

    遇到 vue3 的 bug，component 不会跟随 title 变化

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,28 +1,81 @@
 <template>
-  <div>
-    <div v-for="(t, index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c, index) in defaults" :is="c" :key="index"/>
+  <div class="z-tabs">
+    <div class="z-tabs-nav">
+      <div class="z-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t === selected}"
+           :key="index">{{t}}
+      </div>
+    </div>
+    <div class="z-tabs-content">
+      <component class="z-tabs-content-item" :is="current"/>
+      {{current}}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-  import Tab from './Tab.vue'
+  import Tab from './Tab.vue';
+  import {computed} from 'vue';
 
   export default {
-    setup(props, context){
-      const defaults = context.slots.default()
+    props: {
+      selected: {
+        type: String
+      }
+    },
+    setup(props, context) {
+      const defaults = context.slots.default();
       defaults.forEach((tag) => {
         if (tag.type !== Tab) {
-          throw new Error("Tabs 子标签必须是 Tab")
+          throw new Error('Tabs 子标签必须是 Tab');
         }
-      })
+      });
+      const current = computed(() => {
+        console.log('重新 return');
+        return defaults.filter((tag) => {
+          return tag.props.title === props.selected;
+        })[0];
+      });
       const titles = defaults.map((tag) => {
         return tag.props.title;
-      })
+      });
+      const select = (title: string) => {
+        context.emit('update:selected', title);
+      };
       return {
-        defaults, titles
+        defaults, titles, current, select
+      };
+    }
+
+  };
+</script>
+
+<style lang="scss">
+  $blue: #40a9ff;
+  $color: #333;
+  $border-color: #d9d9d9;
+  .z-tabs {
+    &-nav {
+      display: flex;
+      color: $color;
+      border-bottom: 1px solid $border-color;
+
+      &-item {
+        padding: 8px 0;
+        margin: 0 16px;
+        cursor: pointer;
+
+        &:first-child {
+          margin-left: 0;
+        }
+
+        &.selected {
+          color: $blue;
+        }
       }
     }
 
+    &-content {
+      padding: 8px 0;
+    }
   }
-</script>
+</style>
